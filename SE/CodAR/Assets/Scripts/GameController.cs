04c5c693e27013b50075e7bb00506f24496ca250
Darian <thehelljelp@gmail.com>
2017-12-06 11:38:05 +0100

    KeyLock, Saver, GameController
    Alle hinzugefügt, manche low-level

--- SE/CodAR/Assets/Scripts/GameController.cs
+++ SE/CodAR/Assets/Scripts/GameController.cs
@@ -4,13 +4,49 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public bool items;
+	public bool progression;
+
+
+	void Start() {
+		if (!PlayerPrefs.HasKey ("firstrun")) {
+			
+			PlayerPrefs.SetInt ("firstrun", 1);
+
+			PlayerPrefs.SetInt ("items", 0);
+			items = false;
+
+			PlayerPrefs.SetInt ("progression", 0);
+			progression = false;
+
+			PlayerPrefs.SetInt ("lastscene", 0);
+
+
+		} else {
+			
+			items = GetValue ("items");
+
+			progression = GetValue ("progression");
+
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+
+
+
+
+	bool GetValue(string key) {
+		return (PlayerPrefs.GetInt (key) != 0);
 	}
+
+	public bool CheckItem(string name) {
+		if (name == "screwdriver" && items) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+
+
 }
